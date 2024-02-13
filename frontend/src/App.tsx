9024646dd5b57768ce66c9d06e1cde07b6b35d4f
Luis Roberto <luisrobertopenav@gmail.com>
2024-02-13 12:31:43 -0700

    add prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from "./App.module.scss";
+import React from 'react';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className={styles["placeholder-text"]}>
+    <div className={styles['placeholder-text']}>
       <p>HELLO WORLD!</p>
     </div>
   );
