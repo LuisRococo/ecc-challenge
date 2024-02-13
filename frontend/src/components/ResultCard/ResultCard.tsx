3e5e661f065be1f715473f510394d422ceb81472
Luis Roberto <luisrobertopenav@gmail.com>
2024-02-13 15:01:25 -0700

    add icons

--- frontend/src/components/ResultCard/ResultCard.tsx
+++ frontend/src/components/ResultCard/ResultCard.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './ResultCard.module.scss';
 import Card from '../Card/Card';
+import { TbCircuitResistor } from 'react-icons/tb';
+import { FaDiamond } from 'react-icons/fa6';
 
 const ResultCard = () => {
   return (
     <div className={styles['result-card']}>
       <Card title="Result">
         <div className={styles['result-data']}>
-          <div>
+          <div className={styles['result-data__item']}>
+            <TbCircuitResistor className={styles['result-data__item-icon']} />
+
             <p>
               OHM: <span>200</span>
             </p>
           </div>
 
-          <div>
+          <div className={styles['result-data__item']}>
+            <FaDiamond className={styles['result-data__item-icon']} />
+
             <p>
               Tolerance: <span>±200</span>
             </p>
