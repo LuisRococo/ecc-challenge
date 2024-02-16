3428a74d7f7a4c9999c5f2609ca88c4fa6dfb494
Luis Roberto <luisrobertopenav@gmail.com>
2024-02-16 15:20:14 -0700

    refactor routes and add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import styles from './App.module.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Router from './router';
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/IndexPage/IndexPage';
 
 function App() {
   return (
-    <div className={styles['main-container']}>
-      <div>
-        <Header />
-        <Router />
-      </div>
+    <div>
+      <Navbar />
+      <div className={styles['main-container']}>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/formulas" element={<div>Placeholder</div>} />
+          </Routes>
+        </div>
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 }
